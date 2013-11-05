a9ee60ca5fd09777fe898de10b0b94a409458f11
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2013-11-05 14:06:35 +0100

    Pyramid test .obj file

--- src/scene/C3.js
+++ src/scene/C3.js
@@ -1,52 +1,15 @@
 function loadC3() {
 
-    var sphereMesh1 = readOBJ('./data/sphere.obj')
+    var sphereMesh1 = readOBJ('./data/sphere.obj', $V([0,0,0]), 2);
     sphereMesh1.material = Material.red();
-    sphereMesh1.position = $V([0,0,0]);
-    sphereMesh1.scale = 2;
 
-    //var sphereMesh2 = readOBJ('./data/sphere.obj')
-    //sphereMesh2.material = Material.blue();
-    //sphereMesh2.position = $V([1.25,1.25,3]);
-    //sphereMesh2.scale = 0.5;
+    var sphereMesh2 = readOBJ('./data/sphere.obj', $V([1.25,1.25,3]), 0.5);
+    sphereMesh2.material = Material.blue();
 
     scene.addObject(sphereMesh1);
-    //scene.addObject(sphereMesh2);
+    scene.addObject(sphereMesh2);
 
-    var t1 = new Triangle(
-        $V([0,1,0]),
-        $V([0,0,0]),
-        $V([1,0,0]),
-        Material.red()
-    )
-    var t2 = new Triangle(
-        $V([1,0,0]),
-        $V([1,1,-1]),
-        $V([0,1,0]),
-        Material.red()
-    )
-    var t3 = new Triangle(
-        $V([0,0,0]),
-        $V([0,-1,-0.5]),
-        $V([1,0,0]),
-        Material.red()
-    )
-    var t4 = new Triangle(
-        $V([0,0,0]),
-        $V([0,1,0]),
-        $V([-1,0,-1]),
-        Material.red()
-    )
-    var t5 = new Triangle(
-        $V([0,0,0]),
-        $V([-1,0,-1]),
-        $V([0,-1,-0.5]),
-        Material.red()
-    )
-
-  //  scene.addObject(t1);
-  //  scene.addObject(t2);
-  //  scene.addObject(t3);
-  //  scene.addObject(t4);
-  //  scene.addObject(t5);
+    // var pyr = readOBJ('./data/pyramid.obj', $V([-0.5,-0.5,0]), 3)
+    // pyr.material = Material.blue();
+    // scene.addObject(pyr);
 }
